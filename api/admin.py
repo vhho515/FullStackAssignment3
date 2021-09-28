eb25a10136f9d28258c2fcf8082c8c586d917803
vhho515 <89646443+vhho515@users.noreply.github.com>
2021-09-27 20:40:28 -0500

    Assignment3Ho
    All uploads for assignment 3

---
+++ api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Movie
+
+
+class MovieList(admin.ModelAdmin):
+    list_display = ('name', 'year', 'description', 'rating')
+    list_filter = ('name', 'year', 'rating')
+    search_fields = ('name', 'description')
+    ordering = ['year']
+
+
+admin.site.register(Movie, MovieList)
+
+# Register your models here.
